7edfb935e46dab89ad0f9f7286c122a9231b1011
YazAva1008 <84940361+YazAva1008@users.noreply.github.com>
2021-07-28 18:15:47 -0500

    cambios2

--- app/assets/demo/chart-area-demo.js
+++ app/assets/demo/chart-area-demo.js
@@ -7,17 +7,17 @@ var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
+    labels: ["2018", "2019", "2020", "2021"],
     datasets: [{
       label: "Sessions",
       lineTension: 0.3,
-      backgroundColor: "rgba(2,117,216,0.2)",
-      borderColor: "rgba(2,117,216,1)",
+      backgroundColor: "rgba(221, 117, 0, 0.4)",
+      borderColor: "rgba(221, 117, 0, 1)",
       pointRadius: 5,
-      pointBackgroundColor: "rgba(2,117,216,1)",
+      pointBackgroundColor: "rgba(191, 46, 0, 1)",
       pointBorderColor: "rgba(255,255,255,0.8)",
       pointHoverRadius: 5,
-      pointHoverBackgroundColor: "rgba(2,117,216,1)",
+      pointHoverBackgroundColor: "rgba(191, 46, 0, 1)",
       pointHitRadius: 50,
       pointBorderWidth: 2,
       data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
