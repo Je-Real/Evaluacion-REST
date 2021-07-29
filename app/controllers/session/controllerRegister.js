61fc836fdb30b70694d5260a91f0cadfd3019b38
Jere Os <50892043+0s-el-poderoso@users.noreply.github.com>
2021-07-28 19:27:03 -0500

    Encryption start!
    Let's encrypt some passwords

--- app/controllers/session/controllerRegister.js
+++ app/controllers/session/controllerRegister.js
@@ -1,6 +1,7 @@
 //Import db model
 const path = require('path')
 const modelUser = require('../../models/modelUser')
+const crypto = require('crypto')
 
 // >>>>>>>>>>>>>>>>>>>>>> Charts <<<<<<<<<<<<<<<<<<<<<<
 function root(req, res){
@@ -9,11 +10,44 @@ function root(req, res){
 }
 
 function signIn(req, res){
-    new modelUser(req.body).save()
+    /*
+    // Cifrado de Nodejs con CTR
+    const crypto = require ( 'crypto ');
+    algoritmo const = 'aes-256-cbc ';
+    contrast clave = crypto.randomBytes (32);
+    const iv = crypto.randomBytes (16);
+
+    funcion encriptar (texto) {
+
+    let cipher = crypto.createCipheriv ( 'aes-256-cbc ', Buffer.from (clave), iv);
+    let encrypted = cipher.update (texto);
+    cifrado = Buffer.concat ([cifrado, cifrado.final ()]);
+    return {iv: iv.toString ( 'hex '), encryptedData: encrypted.toString ( 'hex ')};
+    }
+
+    funcion descifrar (texto) {
+
+    let iv = Buffer.from (text.iv, 'hex ');
+    let encryptedText = Buffer.from (text.encryptedData, 'hex ');
+    dejar descifrar = crypto.createDecipheriv ( 'aes-256-cbc ', Buffer.from (clave), iv);
+    dejar descifrado = decipher.update (encryptedText);
+    descifrado = Buffer.concat ([descifrado, descifrado.final ()]);
+    return decrypted.toString
+    }
+    */
+
+    if(req.body.user == null)
+        return res.status(500).render(path.join(__dirname + '/../../views/session/register'))
+
+    var cKey = crypto.createHmac('sha1', req.body.user).update(req.body.pass).digest('hex')
+
+    console.log('Hash:', cKey)
+
+    /*new modelUser(req.body).save()
     .then(data => {
         return res.status(200).render(path.join(__dirname + '/../../views/session/login'))
     })
-    .catch(error => res.status(500).send({error}))
+    .catch(error => res.status(500).send({error}))*/
 }
 
 module.exports = {
