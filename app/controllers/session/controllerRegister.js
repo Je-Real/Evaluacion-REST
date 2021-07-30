fd830b3ced6f3296f7e43e8c401b049813fbe151
Jere Os <50892043+0s-el-poderoso@users.noreply.github.com>
2021-07-29 19:53:35 -0500

    Changes in logo and encryption working
    Yeah!

--- app/controllers/session/controllerRegister.js
+++ app/controllers/session/controllerRegister.js
@@ -1,7 +1,9 @@
 //Import db model
 const path = require('path')
 const modelUser = require('../../models/modelUser')
-const crypto = require('crypto');
+const crypto = require('crypto')
+const key = crypto.randomBytes(32)
+const iv = crypto.randomBytes(16)
 
 // >>>>>>>>>>>>>>>>>>>>>> Charts <<<<<<<<<<<<<<<<<<<<<<
 function root(req, res){
@@ -9,35 +11,38 @@ function root(req, res){
     return res.status(200).render(path.join(__dirname + '/../../views/session/register'))
 }
 
-function signIn(req, res){
-    var iv = crypto.randomBytes(16)
+async function signIn(req, res){
+    modelUser.find(req.body.user)
+    .then(data => {
+        if(data.length) { //if data 👍
+            return console.log('Existe usuario', data)
+            //req.body.userdata = data
+        }
+        else { //if no data 🥶
+            console.log('No data')
+            //return next()
+        }
+    })
+    .catch(error => { //if error 🤬
+        //req.body.error = error
+        //next()
+        console.log('Error')
+    })
 
-    //var mykey = crypto.createCipher('aes-128-cbc', 'mypassword');
-    var key = crypto.createCipheriv('aes-256-gcm', crypto.randomBytes(32), iv)
-    var eKey = key.update(req.body.pass, 'utf8', 'hex')
-    ekey += key.final('hex');
-
-    console.log('Encrypted:', eKey)
-
-
-    var mykey = crypto.createDecipheriv('aes-128-cbc', crypto.randomBytes(32), iv);
-    var mystr = mykey.update(eKey, 'hex', 'utf8')
-    mystr += mykey.final('utf8');
-
-    console.log('Decrypted:', mystr)
+    var cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv)
+    var encrypted = cipher.update(req.body.pass)
+    encrypted = Buffer.concat([encrypted, cipher.final()])
+    await console.log('Encrypted:', encrypted.toString('hex'))
 
     if(req.body.user == null)
         return res.status(500).render(path.join(__dirname + '/../../views/session/register'))
 
-    //var cKey = crypto.createHmac('sha1', req.body.user).update(req.body.pass).digest('hex')
-
-    //console.log('Hash:', cKey)
-
-    /*new modelUser(req.body).save()
+    new modelUser(req.body).save()
     .then(data => {
+        console.log(data)
         return res.status(200).render(path.join(__dirname + '/../../views/session/login'))
     })
-    .catch(error => res.status(500).send({error}))*/
+    .catch(error => res.status(500).send({error}))
 }
 
 module.exports = {
