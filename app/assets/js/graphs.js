0caf6deab72d31a2c67a78f1562f4952df9272ed
Jere Os <elit1403@hotmail.com>
2021-09-20 23:33:34 -0500

    Added graphs to the other cards in reports page
    Shows all the different graphs needed in Reportes.
    Fixed some bugs in notifications.

---
+++ app/assets/js/graphs.js
@@ -0,0 +1,136 @@
+function doughnutChart(labels, data, colors) {
+    if(labels.length && data.length){
+        var ctx = document.getElementById("doughnutChart").getContext("2d")
+        var doughnutChart = new Chart(ctx, {
+            type: "doughnut",
+            data: {
+                labels: labels, //Years labels
+                datasets: [
+                    {
+                        label: "Puntuación",
+                        data: data, //Records' data
+                        backgroundColor: [
+                            "rgba(255, 99, 132, 0.65)",
+                            "rgba(54, 162, 235, 0.65)",
+                            "rgba(255, 206, 86, 0.65)",
+                            "rgba(75, 192, 192, 0.65)",
+                            "rgba(153, 102, 255, 0.65)",
+                            "rgba(255, 159, 64, 0.65)",
+                        ],
+                        borderColor: [
+                            "rgba(255, 99, 132, 1)",
+                            "rgba(54, 162, 235, 1)",
+                            "rgba(255, 206, 86, 1)",
+                            "rgba(75, 192, 192, 1)",
+                            "rgba(153, 102, 255, 1)",
+                            "rgba(255, 159, 64, 1)",
+                        ],
+                        borderWidth: 2,
+                    },
+                ],
+            }
+        })
+        console.log('Loaded doughnut!')
+    }
+}
+
+function barChart(labels, data, colors) {
+    if(labels.length && data.length){
+        var ctx = document.getElementById("barChart").getContext("2d")
+        var barChart = new Chart(ctx, {
+            type: "bar",
+            data: {
+                labels: labels, //Years labels
+                datasets: [
+                    {
+                        label: "Puntuación",
+                        data: data, //Records' data
+                        backgroundColor: [
+                            "rgba(255, 99, 132, 0.65)",
+                            "rgba(54, 162, 235, 0.65)",
+                            "rgba(255, 206, 86, 0.65)",
+                            "rgba(75, 192, 192, 0.65)",
+                            "rgba(153, 102, 255, 0.65)",
+                            "rgba(255, 159, 64, 0.65)",
+                        ],
+                        borderColor: [
+                            "rgba(255, 99, 132, 1)",
+                            "rgba(54, 162, 235, 1)",
+                            "rgba(255, 206, 86, 1)",
+                            "rgba(75, 192, 192, 1)",
+                            "rgba(153, 102, 255, 1)",
+                            "rgba(255, 159, 64, 1)",
+                        ],
+                        borderWidth: 2,
+                    },
+                ],
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                    },
+                },
+            },
+        })
+        console.log('Loaded bars!')
+    }
+}
+
+function lineChart(labels, data, colors) {
+    if(labels.length && data.length){
+        var ctx = document.getElementById("lineChart").getContext("2d")
+        var lineChart = new Chart(ctx, {
+            type: "line",
+            data: {
+                labels: labels, //Years labels
+                datasets: [
+                    {
+                        label: "Puntuación",
+                        data: data, //Records' data
+                        cubicInterpolationMode: 'monotone',
+                        fill: false,
+                        tension: 0.4,
+                        borderColor: function(context) {
+                            const chart = context.chart;
+                            const {ctx, chartArea} = chart;
+                    
+                            if (!chartArea) {
+                              // This case happens on initial chart load
+                              return null;
+                            }
+                            return getGradient(ctx, chartArea);
+                        },
+                        borderWidth: 3,
+                    },
+                ],
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                    },
+                },
+            },
+        })
+        console.log('Loaded line!')
+    }
+}
+
+let width, height, gradient;
+function getGradient(ctx, chartArea) {
+  const chartWidth = chartArea.right - chartArea.left;
+  const chartHeight = chartArea.bottom - chartArea.top;
+  if (gradient === null || width !== chartWidth || height !== chartHeight) {
+    // Create the gradient because this is either the first render
+    // or the size of the chart has changed
+    width = chartWidth;
+    height = chartHeight;
+    gradient = ctx.createLinearGradient(0, chartArea.bottom, 0, chartArea.top)
+    gradient.addColorStop(0, '#ED859A')
+    gradient.addColorStop(0.5, '#FFD452')
+    gradient.addColorStop(1, '#84ECB6')
+  }
+
+  return gradient;
+}
--- app/assets/js/reportes.js
+++ app/assets/js/reportes.js
@@ -1,101 +1,66 @@
 $(document).ready(() => {
-    $('.dep').addClass('d-none') //Hide department options
-    $('#department').prop('disabled', true) //Disable dropdown for department
+	$(".dep").addClass("d-none") //Hide department options
+	$("#department").prop("disabled", true) //Disable dropdown for department
 })
 
-$('#area').change(() => {
-    $('#dep-s').addClass('d-none') //Hide default option
-    $('.dep').addClass('d-none') ////Hide again all department options
+$("#area").change(() => {
+	$("#dep-s").addClass("d-none") //Hide default option
+	$(".dep").addClass("d-none") ////Hide again all department options
 
-    //And show all departments the ones that match with the area selected
-    var affected = $(`.dep[data-area="${parseInt($('#area').val())}"]`)
-        .removeClass('d-none')
+	//And show all departments the ones that match with the area selected
+	var affected = $(`.dep[data-area="${parseInt($("#area").val())}"]`).removeClass("d-none")
 
-    if (affected.length == 0) { //If in the area does not exist any departments
-        $('#dep-s').text('N/A')
-            .removeClass('d-none')
-            .prop('selected', true)
-        $('#department').prop('disabled', true)
-    } else { //Else 🟢
-        $('#dep-s').text('-Selecciona departamento-')
-            .removeClass('d-none')
-            .prop('selected', true)
-        $('#department').prop('disabled', false)
-    }
+	if (affected.length == 0) {
+		//If in the area does not exist any departments
+		$("#dep-s").text("N/A").removeClass("d-none").prop("selected", true)
+		$("#department").prop("disabled", true)
+	} else {
+		//Else 🟢
+		$("#dep-s").text("-Selecciona departamento-").removeClass("d-none").prop("selected", true)
+		$("#department").prop("disabled", false)
+	}
 })
 
-$('#department').change(() => {
-    $('#dep-s').addClass('d-none') //Hide default option
+$("#department").change(() => {
+	$("#dep-s").addClass("d-none") //Hide default option
 })
 
-$('.form-select').change(() => { //Get reports
-    area = parseInt($('#area').val())
-    depart = (parseInt($('#department').val()) > 0) ? parseInt($('#department').val()) : null
+$(".form-select").change(() => {
+	//Get reports
+	area = parseInt($("#area").val())
+	depart = parseInt($("#department").val()) > 0 ? parseInt($("#department").val()) : null
 
-    if (parseInt($('#area').val()) > 0) {
-        $.ajax({
-            type: 'POST',
-            url: 'http://localhost:3000/reportes/get',
-            contentType: 'application/json; charset=utf-8',
-            data: JSON.stringify({
-                area: area,
-                department: depart,
-                _id: user
-            }),
-            dataType: 'json',
-            async: true,
-            success: function (result) {
-                if (result.status === 200) {
-                    showSnack(result.msg, 'success')
-                    var years = [], records = []
-                    for(var r in result.data[0].records) {
-                        years.push(String(r))
-                        records.push(result.data[0].records[r])
-                    }
+	if (parseInt($("#area").val()) > 0) {
+		$.ajax({
+			type: "POST",
+			url: "http://localhost:3000/reportes/get",
+			contentType: "application/json; charset=utf-8",
+			data: JSON.stringify({
+				area: area,
+				department: depart,
+				_id: user,
+			}),
+			dataType: "json",
+			async: true,
+			success: function (result) {
+				if (result.status === 200) {
+					var years = [], records = []
 
-                    var ctx = document.getElementById('pieChart').getContext('2d')
-                    var pieChart = new Chart(ctx, {
-                        type: 'bar',
-                        data: {
-                            labels: years, //Modify
-                            datasets: [{
-                                label: '# of Votes',
-                                data: records, //Modify
-                                backgroundColor: [
-                                    'rgba(255, 99, 132, 0.2)',
-                                    'rgba(54, 162, 235, 0.2)',
-                                    'rgba(255, 206, 86, 0.2)',
-                                    'rgba(75, 192, 192, 0.2)',
-                                    'rgba(153, 102, 255, 0.2)',
-                                    'rgba(255, 159, 64, 0.2)'
-                                ],
-                                borderColor: [
-                                    'rgba(255, 99, 132, 1)',
-                                    'rgba(54, 162, 235, 1)',
-                                    'rgba(255, 206, 86, 1)',
-                                    'rgba(75, 192, 192, 1)',
-                                    'rgba(153, 102, 255, 1)',
-                                    'rgba(255, 159, 64, 1)'
-                                ],
-                                borderWidth: 1
-                            }]
-                        },
-                        options: {
-                            scales: {
-                                y: {
-                                    beginAtZero: true
-                                }
-                            }
-                        }
-                    })
-                } else {
-                    showSnack(result.msg, 'danger')
-                }
-            },
-            error: function (xhr, status, error) {
-                showSnack('Status: ' + status + '. ' + error, 'error')
-            }
-        })
-    }
+					for (var r in result.data[0].records) {
+						years.push(String(r))
+						records.push(result.data[0].records[r])
+					}
+
+                    doughnutChart(years, records)
+                    barChart(years, records)
+                    lineChart(years, records)
+				} else {
+					showSnack(result.msg, "danger")
+				}
+			},
+			error: function (xhr, status, error) {
+				showSnack("Status: " + status + ". " + error, "error")
+			},
+		})
+	}
 })
-
