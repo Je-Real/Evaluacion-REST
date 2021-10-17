0f6c6fe39d48212c4a4aa11b5f7418cd8e290acc
Jere Os <elit1403@hotmail.com>
2021-10-17 17:14:13 -0500

    Fixing Registro page
    Adding options for levels in new users and displaying the relevant manager for each options (area, department, career & level)

--- app/assets/js/registro.js
+++ app/assets/js/registro.js
@@ -1,20 +1,103 @@
-var proceed = document.getElementById('submit')
-var firstName, lastName, area, career, state, municipality, street, number, postal_code
+var firstName, lastName,
+    area, career, state,
+    municipality, street,
+    number, postal_code,
+    lvl_s = 0
 
-window.addEventListener('DOMContentLoaded', event => {
-    proceed.disabled = true
+$(document).ready(async() => {
+    $('.dep').addClass('d-none') //Hide department options
+	$('#department').prop('disabled', true) //Disable dropdown for department
+
+    $('.car').addClass('d-none') //Hide department options
+	$('#career').prop('disabled', true) //Disable dropdown for careers
+
+    $('#contract').prop('disabled', true) //Disable dropdown for contracts
+    $('#lvl').prop('disabled', true) //Disable dropdown for levels
+
+    levels(0, 0)
 })
 
+$('#area').change(() => {
+	$('#dep-s').addClass('d-none') //Hide default option
+	$('.dep').addClass('d-none') ////Hide again all department options
+
+	//And show all departments the ones that match with the area selected
+	var affected = $(`.dep[data-area='${parseInt($('#area').val())}']`).removeClass('d-none')
+
+	if (affected.length == 0) {
+		//If in the area does not exist any departments
+		$('#dep-s').text('N/A').removeClass('d-none').prop('selected', true)
+		$('#department').prop('disabled', true)
+        $('#contract').prop('disabled', true)
+	} else {
+		//Else 🟢
+		$('#dep-s').text('-Selecciona carrera-').removeClass('d-none').prop('selected', true)
+		$('#department').prop('disabled', false)
+        $('#contract').prop('disabled', false)
+	}
+    levels(1, $('#area').val())
+})
+
+$('#department').change(() => {
+	$('#car-s').addClass('d-none') //Hide default option
+	$('.car').addClass('d-none') ////Hide again all department options
+
+	//And show all departments the ones that match with the area selected
+	var affected = $(`.car[data-depa='${parseInt($('#department').val())}']`).removeClass('d-none')
+
+	if (affected.length == 0) {
+		//If in the area does not exist any departments
+		$('#car-s').text('N/A').removeClass('d-none').prop('selected', true)
+		$('#career').prop('disabled', true)
+	} else {
+		//Else 🟢
+		$('#car-s').text('-Selecciona departamento-').removeClass('d-none').prop('selected', true)
+		$('#career').prop('disabled', false)
+	}
+    levels(2, $('#department').val())
+})
+
+$('#career').change(() => {
+    levels(3, $('#career').val())
+})
+
+function levels(lvl, val) {
+    var lvl_temp = lvl_s 
+
+    lvl_s = (parseInt(val)===0 && parseInt(lvl)<=1)
+        ? 0 : ((parseInt(val)===0 && parseInt(lvl)===2)
+            ? 1 : ((parseInt(val)===0 && parseInt(lvl)===3)
+                ? 2 : ((parseInt(val)>0 && parseInt(lvl)!=lvl_s)
+                    ? parseInt(lvl) : lvl_s)))
+
+    if(lvl_temp != lvl_s) {
+        $('#lvl').prop('disabled', false)
+        $('.lvl-s').addClass('d-none')
+        $('#lvl-s').text('-Selecciona nivel-').prop('selected', true)
+
+        if(lvl_s === 1) {
+            $('.lvl-s.lvl-1').removeClass('d-none')
+        } else if (lvl_s === 2) {
+            $('.lvl-s.lvl-2').removeClass('d-none')
+        } else if (lvl_s === 3) {
+            $('.lvl-s.lvl-3').removeClass('d-none')
+        } else {
+            $('#lvl').prop('disabled', true)
+            $('#lvl-s').text('N/A').prop('selected', true)
+        }
+    }
+}
+
 function addressGetter() {
-    firstName = String(document.getElementById('first_name').value).trim()
-    lastName = String(document.getElementById('last_name').value).trim()
-    area = document.getElementById('area').value
-    career = document.getElementById('career').value
-    state = String(document.getElementById('state').value).trim()
-    municipality = String(document.getElementById('municipality').value).trim()
-    street = String(document.getElementById('street').value).trim()
-    number = String(document.getElementById('number').value).trim()
-    postal_code = String(document.getElementById('postal_code').value).trim()
+    firstName = String($('#first_name').val()).trim()
+    lastName = String($('#last_name').val()).trim()
+    area = $('#area').val()
+    career = $('#career').val()
+    state = String($('#state').val()).trim()
+    municipality = String($('#municipality').val()).trim()
+    street = String($('#street').val()).trim()
+    number = String($('#number').val()).trim()
+    postal_code = String($('#postal_code').val()).trim()
     
     /*document.getElementById('address').value = 
         String(street+' #'+number+', '+postal_code+', '+municipality+', '+state)*/
@@ -23,23 +106,23 @@ function addressGetter() {
     
     if( firstName.length && lastName.length && area.length && career.length && state.length &&
         municipality.length && street.length && number.length && postal_code.length) {
-            proceed.disabled = false
+            $('#submit').prop('disabled', false)
     }
 }
 
 function register() {
-    var _id = document.getElementById('_id_r').value
-    var fn = document.getElementById('first_name').value
-    var ln = document.getElementById('last_name').value
-    var area = document.getElementById('area').value
-    var dep = document.getElementById('department').value
-    var cr = document.getElementById('career').value
-    var ct = document.getElementById('contract').value
-    var st = document.getElementById('street').value
-    var num = document.getElementById('num').value
-    var pc = document.getElementById('postal_code').value
-    var bday = document.getElementById('b_day').value
-    var pass = document.getElementById('pass_r').value
+    var _id = $('#_id_r').val()
+    var fn = $('#first_name').val()
+    var ln = $('#last_name').val()
+    var area = $('#area').val()
+    var dep = $('#department').val()
+    var cr = $('#career').val()
+    var ct = $('#contract').val()
+    var st = $('#street').val()
+    var num = $('#num').val()
+    var pc = $('#postal_code').val()
+    var bday = $('#b_day').val()
+    var pass = $('#pass_r').val()
 
     var packed = JSON.stringify({ 
         _id: _id, 
@@ -75,3 +158,9 @@ function register() {
         }
     })
 }
+
+
+/**
+ * Hacer funcion AJAX para busqueda automatica de Manager
+ * dependiendo el nivel del nuevo usuario
+ */
