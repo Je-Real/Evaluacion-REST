5a1892dc01edfe36da16f11a899b24839595a646
Jere Os <50892043+0s-el-poderoso@users.noreply.github.com>
2021-10-26 09:42:39 -0500

    Added Controller for Reset Password

---
+++ app/controllers/session/controllerResetPsw.js
@@ -0,0 +1,51 @@
+const modelUser = require('../../models/modelUser')
+const crypto = require('crypto-js')
+
+// >>>>>>>>>>>>>>>>>>>>>> Login <<<<<<<<<<<<<<<<<<<<<<
+async function reset(req, res) {
+	await modelUser.find({ _id: req.body._id })
+	.then((dataUser) => {
+		if (dataUser.length) { //if data 👍
+            //Encryption
+			dataUser.pass = crypto.AES.encrypt(req.body.pass, req.body._id).toString()
+
+			//Save data
+			new modelUser(dataUser).save()
+			.then(() => { //🟢
+				return res.end(JSON.stringify({
+					msg: '¡Contraseña actualizada correctamente!',
+					status: 200,
+					noti: true
+				}))
+			})
+			.catch((error) => { //🔴
+				console.log(error)
+				return res.end(JSON.stringify({
+					msg: 'No se puede actualizar contraseña.\r\nIntentalo más tarde.',
+					status: 500,
+					noti: true
+				}))
+			})
+		} else { //if no data 🥶
+			return res.end(JSON.stringify({
+				msg: '¡No se encontro usuario!',
+				status: 404,
+				noti: true
+			}))
+		}
+	})
+	.catch((error) => { //if error 🤬
+		console.log('Error:',error)
+		return res.end(JSON.stringify({
+			msg: 'Error en servidor.',
+			status: 500,
+			noti: true
+		}))
+	})
+	//NUNCA colocar un return fuera del catch
+	//NEVER place a return outside the catch
+}
+
+module.exports = {
+	reset,
+}
